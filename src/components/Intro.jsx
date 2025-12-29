71ddd23d3fc99245213745e4b3f0e5098af51fe9
Max Hope-Carter <90761872+mathmaxyz@users.noreply.github.com>
2025-12-29 17:15:54 +0000

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function intro() {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-        <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Helena</h1>
+        <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Max</h1>
         <p className="text-base md:text-xl mb-3 font-medium">Full stack Engineer</p>
         <p className="text-sm max-w-xl mb-6 font-bold">I am a software engineer with three years experience. I am currently looking for work after 2 years at Universal Music Group as a full stack developer. I worked with Java, Spring, Angular, Ionic and AWS in that role. My biggest acheivement was completing an ionic app rewrite and new identity provider integration in half the time estimated by the senior frontend engineer on my team, therefore doubling productivity. I'm hoping to keep stacking up sucesses like that in my next role, and to learn some new technologies.
         Feel free to peruse my website and{' '}
